c1a38ec27c5edb9fd98d663ad031e848db28599b
Pruthvi <127245022+pruthvi423186@users.noreply.github.com>
2026-07-10 16:36:28 +0530

    Simplify model loading and image paths

--- Python/Pred_single_img.py
+++ Python/Pred_single_img.py
@@ -1,17 +1,17 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO(r"D:\Downloads\runs\detect\runs\train\yolo26n_custom\weights\best.pt")  # pretrained YOLO26n model
+model = YOLO(r"best.pt")  
 
 # Run batched inference on a list of images
-results = model(["D:\Downloads\image1.jpg"], stream=True)  # return a generator of Results objects
+results = model(["image1.jpg"], stream=True)  
 
 # Process results generator
 for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
-    result.show()  # display to screen
-    result.save(filename="result.jpg")  # save to disk
+    boxes = result.boxes  
+    masks = result.masks  
+    keypoints = result.keypoints  
+    probs = result.probs 
+    obb = result.obb  
+    result.show()  
+    result.save(filename="result.jpg") 
